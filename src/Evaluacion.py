4c3c22e99d300dbda2754c7d52f7035bb67033ce
AlbertoSierraLopez <alberto.sierra360@outlook.com>
2021-05-27 14:32:30 +0200

    Output txt

--- src/Evaluacion.py
+++ src/Evaluacion.py
@@ -8,13 +8,13 @@ class Evaluacion:
         report = classification_report(y_test, y_predicted, output_dict=True, zero_division=1)
         print()
         if accuracy:
-            print("\t- Accuracy:",   round(report["accuracy"]                  * 100, 2), '%')
+            print("\t- Accuracy:",  round(report["accuracy"]                  * 100, 2), '%')
         if precision:
-            print("\t- Precision:",   round(report["weighted avg"]["precision"] * 100, 2), '%')
+            print("\t- Precision:", round(report["weighted avg"]["precision"] * 100, 2), '%')
         if recall:
-            print("\t- Recall:",     round(report["weighted avg"]["recall"]    * 100, 2), '%')
+            print("\t- Recall:",    round(report["weighted avg"]["recall"]    * 100, 2), '%')
         if f1:
-            print("\t- F1 score:", round(report["weighted avg"]["f1-score"]  * 100, 2), '%')
+            print("\t- F1 score:",  round(report["weighted avg"]["f1-score"]  * 100, 2), '%')
 
 
     def plot_matrix(self, clf, X_test, y_test):
